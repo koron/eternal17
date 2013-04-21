9fc83c2cad88dd18b632701f275b4552c3510b8b
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-04-21 18:26:18 +0900

    impl calculation

---
+++ eternal17/src/net/kaoriya/eternal17/Age.java
@@ -0,0 +1,18 @@
+package net.kaoriya.eternal17;
+
+public final class Age
+{
+
+    public int years = 17;
+    public int days = 0;
+
+    public Age(int years, int days)
+    {
+        this.years = years;
+        this.days = days;
+    }
+
+    public Age()
+    {
+    }
+}
--- eternal17/src/net/kaoriya/eternal17/MainActivity.java
+++ eternal17/src/net/kaoriya/eternal17/MainActivity.java
@@ -1,7 +1,12 @@
 package net.kaoriya.eternal17;
 
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
@@ -10,5 +15,41 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        // TODO: configurable birth day.
+        Date birthDay = new GregorianCalendar(1970, 1, 1).getTime();
+        setAge(calcAge(birthDay));
+    }
+
+    private void setAge(Age age)
+    {
+        if (age == null) {
+            age = new Age();
+        }
+        TextView textYears = (TextView)findViewById(R.id.text_years);
+        textYears.setText(getString(R.string.format_years, age.years));
+        TextView textDays = (TextView)findViewById(R.id.text_days);
+        textDays.setText(getString(R.string.format_days, age.days));
+    }
+
+    public static Age calcAge(Date birthDay)
+    {
+        Calendar c = Calendar.getInstance();
+        c.clear();
+        c.setTime(birthDay);
+        c.add(Calendar.YEAR, 17);
+        int days = getDays(new Date(), c.getTime());
+        return new Age(17, days);
+    }
+
+    public static int getDays(Date d1, Date d0)
+    {
+        long diff = d1.getTime() - d0.getTime();
+        return (int)(diff / 86400000L);
     }
 }
