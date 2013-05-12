5c7761130558e1d77cdb7b17177fae7c6ebd69d6
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-05-13 00:00:22 +0900

    choosable birthday

--- eternal17/src/net/kaoriya/eternal17/MainActivity.java
+++ eternal17/src/net/kaoriya/eternal17/MainActivity.java
@@ -4,12 +4,20 @@ import java.util.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import android.util.Log;
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.content.SharedPreferences;
 import android.widget.TextView;
+import android.widget.DatePicker;
 
 public class MainActivity extends Activity
 {
+    public final static String TAG = "eternal17";
+    public final static String PREF_SETTINGS = "settings";
+    public final static String BIRTH_DAY = "birth_day";
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -21,9 +29,13 @@ public class MainActivity extends Activity
     public void onResume()
     {
         super.onResume();
-        // TODO: configurable birth day.
-        Date birthDay = new GregorianCalendar(1970, 1, 1).getTime();
-        setAge(calcAge(birthDay));
+
+        Date birthDay = getBirthDay();
+        if (birthDay != null) {
+            setAge(calcAge(birthDay));
+        } else {
+            chooseBirthDay();
+        }
     }
 
     private void setAge(Age age)
@@ -51,5 +63,53 @@ public class MainActivity extends Activity
     {
         long diff = d1.getTime() - d0.getTime();
         return (int)(diff / 86400000L);
+    }
+
+    private Date getBirthDay()
+    {
+        SharedPreferences pref =
+            getSharedPreferences(PREF_SETTINGS, MODE_PRIVATE);
+        long value = pref.getLong(BIRTH_DAY, -1);
+        if (value < 0) {
+            return null;
+        }
+        return new Date(value);
+    }
+
+    private void setBirthDay(Date date)
+    {
+        SharedPreferences.Editor edit =
+            getSharedPreferences(PREF_SETTINGS, MODE_PRIVATE).edit();
+        edit.putLong(BIRTH_DAY, date.getTime());
+        edit.commit();
+    }
+
+    private void applyBirthDay(Date date)
+    {
+        setBirthDay(date);
+        setAge(calcAge(date));
+    }
+
+    private void chooseBirthDay()
+    {
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.YEAR, -17);
+        DatePickerDialog d = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(
+                        DatePicker view,
+                        int year,
+                        int monthOfYear,
+                        int dayOfMonth)
+                    {
+                        applyBirthDay(new GregorianCalendar(year,
+                                monthOfYear, dayOfMonth).getTime());
+                    }
+                },
+                c.get(Calendar.YEAR),
+                c.get(Calendar.MONTH),
+                c.get(Calendar.DAY_OF_MONTH));
+        d.show();
     }
 }
